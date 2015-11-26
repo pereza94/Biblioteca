91bb60c777feefcb04dcd33c71f12e02b2ca366e
pereza94 <perezalejandroes@gmail.com>
2015-11-25 21:46:59 -0300

    Insertar ejemplar terminado

--- Biblioteca/src/entity/Ejemplar.java
+++ Biblioteca/src/entity/Ejemplar.java
@@ -1,8 +1,13 @@
 package entity;
 
+import java.sql.Date;
+
 public class Ejemplar {
 	private Integer numInventario;
 	private boolean estado;
+	private String isbn;
+	private String codUbicaion;
+	private Date fechaAlta;
 	public Integer getNumInventario() {
 		return numInventario;
 	}
@@ -14,6 +19,24 @@ public class Ejemplar {
 	}
 	public void setEstado(boolean estado) {
 		this.estado = estado;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getCodUbicaion() {
+		return codUbicaion;
+	}
+	public void setCodUbicaion(String codUbicaion) {
+		this.codUbicaion = codUbicaion;
+	}
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
 	}
 	
 	
